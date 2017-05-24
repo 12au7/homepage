1295195875b57fcf448c274537713ec975afab7f
12au7 <starwarsdave@gmail.com>
2017-05-24 18:39:24 -0400

    added fancy animations and javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
+$(".search").focus();
+
+jQuery.fn.removeClassExcept = function (val) {
+    return this.each(function (index, el) {
+        var keep = val.split(" "),  // list we'd like to keep
+        reAdd = [],          // ones that should be re-added if found
+        $el = $(el);       // element we're working on
+        // look for which we re-add (based on them already existing)
+        for (var c = 0; c < keep.length; c++){
+          if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
+        }
+
+        // drop all, and only add those confirmed as existing
+        $el
+          .removeClass()               // remove existing classes
+          .addClass(reAdd.join(' '));  // re-add the confirmed ones
+    });
+};
+
+$(document).on('keyup',function(evt) {
+    if (evt.keyCode == 27) {
+    $(".secondul,.class-main,.class-2").removeClass("hover1 dropdown clicked");
+    }
+});
+
 $(".class-main").click(function(){
 	$(".secondul", this).toggleClass("dropdown");
 	$(this).toggleClass("clicked");
 });
-$(".search").focus();
+$(".class-main").hover(function(){
+	$(this).toggleClass("hover1");
+    $(".secondul", this).toggleClass("tempdrop");
+});
+$(".class-2").hover(function(){
+	$(this).toggleClass("hover2");
+});
+$(".mainul").mouseleave(function(){
+    $(".class-main").removeClass("hover1 dropdown");
+});
